8bf9468d58014c6826d11c848038935dfad942a2
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-08 21:51:14 +0530

    Add data points

--- data.js
+++ data.js
@@ -45,8 +45,8 @@ const data = {
         ]
     },
     projects: [
-        { id: 1, name: "Iron Learn", desc: "A pure Rust Machine Learning Library", type: "Rust", url: "https://github.com/Palash90/iron_learn" },
-        { id: 1, name: "HDL Emulator", desc: "An online Web Based HDL Emulator for Teaching Purpose", type: "Component", url: "/component/hdlEmulator" }
+        { id: 1, name: "Iron Learn", desc: "A pure Rust Machine Learning Library", type: "rust", url: "https://github.com/Palash90/iron_learn" },
+        { id: 1, name: "HDL Emulator", desc: "An online Web Based HDL Emulator for Teaching Purpose", type: "react", url: "/component/hdlEmulator" }
     ],
     about: {
         jobs: [
