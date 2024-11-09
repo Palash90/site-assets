897c6b46c6d6608e850834714f18d123e6bf6adb
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-09 22:18:50 +0530

    Rename fields

--- data.js
+++ data.js
@@ -35,14 +35,27 @@ const data = {
             {
                 id: "static-file-hosting",
                 title: "Simple Static File Hosting",
-                url: "https://palash90.github.io/site-assets/blogs/static-file-hosting/README.md",
+                mdUrl: "https://palash90.github.io/site-assets/blogs/static-file-hosting/README.md",
                 publishDate: "November 08, 2024",
                 type: "markdown"
             }
         ],
         music: [
-            { id: 1, publishDate: "Jan 2014", title: "Youtube", url: "hMBpAPGqX8k", type: "video" },
-            { id: 2, publishDate: "Jan 2014", title: "Both", video: "hMBpAPGqX8k", md: "https://palash90.github.io/site-assets/blogs/static-file-hosting/README.md", type: "both" }
+            {
+                id: 1,
+                publishDate: "Jan 2014",
+                title: "Youtube",
+                videoId: "hMBpAPGqX8k",
+                type: "video"
+            },
+            {
+                id: 2,
+                publishDate: "Jan 2014",
+                title: "Both",
+                videoId: "hMBpAPGqX8k",
+                mdUrl: "https://palash90.github.io/site-assets/blogs/static-file-hosting/README.md",
+                type: "both"
+            }
         ]
     },
     projects: [
