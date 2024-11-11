125b7904fcf20b371b86161e479ed36adaed670a
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-11 20:00:38 +0530

    Add another tech blog theme

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ const data = {
             sweHeadColor: "#74b9ff",
             musicHeadColor: "#00d1d1",
             itemsPerPage: 50,
-            techBlogClass: "tech-blog-content",
+            techBlogClass: "tech-blog-2",
             musicBlogClass: "music-content",
             genericBlogClass: "blog-content",
             linkClass: "tech-blog-content"
