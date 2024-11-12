9769736038cc685a5cee70ef6828d186f527ccc6
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-12 15:50:24 +0530

    Trying to make sitemap slightly readable

--- site-contents/prepareSitemap.js
+++ site-contents/prepareSitemap.js
@@ -24,10 +24,17 @@ const allLinks = [...staticLinks, ...allNavLinks, ...allBlogs, ...allProjects]
 
 var siteMap = '<?xml version="1.0" encoding="utf-8"?>'
 siteMap += '<?xml-stylesheet type="text/xsl" href="https://palash90.github.io/site-assets/vanilla-water.xslt" ?>'
+siteMap += '\n'
 siteMap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+siteMap += '\n'
 
 allLinks.map(l => {
-    siteMap += "<url><loc>" + l + "</loc></url>"
+    siteMap += "\t<url>"
+    siteMap += '\n'
+    siteMap += '\t\t<loc>' + l + '</loc>'
+    siteMap += '\n'
+    siteMap += '\t</url>'
+    siteMap += '\n'
 })
 
 siteMap += "</urlset>"
