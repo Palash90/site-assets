4f674497fac1ab1721e0d8312805481cf82ce620
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-12 14:14:48 +0530

    Remove unnecessary code from generated js

--- consolidated_script.js
+++ consolidated_script.js
@@ -266,6 +266,8 @@ var findPropsStr = fs.readFileSync("./site-contents/js/findProps.js");
 var code = "const data = JSON.parse(" + json + ")" + "\n"
 
 code += findPropsStr + "\n"
+code +="console.log(findProp('pages.home.desc'))"
+
 
 console.log("Writing consolidated code to file")
 fs.writeFileSync("script.js", code)
