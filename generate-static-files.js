d76bd72659cd69b709a55d9d7c8e6c0f6a5f3ca1
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-13 02:48:42 +0530

    Add common markdown

--- generate-static-files.js
+++ generate-static-files.js
@@ -22,6 +22,7 @@ var jsFiles = [
 
 var cssFiles = [
     "./site-contents/stylesheets/app.css",
+    "./site-contents/stylesheets/markdown.css",
     "./site-contents/stylesheets/markdown-tomato.css",
     "./site-contents/stylesheets/about.css",
     "./site-contents/stylesheets/tech-blog.css",
