b19084fcd60f37c71e3919e2b4727662329d2be7
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-13 03:16:23 +0530

    Cyan theme

--- site-contents/js/common.js
+++ site-contents/js/common.js
@@ -2,8 +2,8 @@ const staticMap = {
     cdn: "https://palash90.github.io/site-assets",
     path_var: "/#",
     raw_content_cdn: "https://raw.githubusercontent.com/Palash90",
-    themeHeaderColor: "tomato",
-    themePColor: "orange"
+    themeHeaderColor: "#6CA1D1",
+    themePColor: "#A5D8E9"
 }
 
 const getCommon = (key) => staticMap[key]
