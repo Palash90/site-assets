12bf97645376797d7c38eb4d4c6570af8421d5d8
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-12 13:33:02 +0530

    Fix the script issue

--- script.js
+++ script.js
@@ -247,6 +247,16 @@ const data = {
 }
 
 
+function findProp(prop) {
+    var obj = data
+    prop = prop.split('.');
+    for (var i = 0; i < prop.length; i++) {
+        if (typeof obj[prop[i]] == 'undefined')
+            return null;
+        obj = obj[prop[i]];
+    }
+    return obj;
+}
 const fs = require('fs');
 
 const json = JSON.stringify(JSON.stringify(data))
