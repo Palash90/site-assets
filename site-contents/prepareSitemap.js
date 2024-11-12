a15ba6a943eb0fdfe8a1c3f5ff708464eee1befc
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-12 15:22:29 +0530

    Adding sitemap style

--- site-contents/prepareSitemap.js
+++ site-contents/prepareSitemap.js
@@ -22,7 +22,8 @@ const allProjects = findProp("projects").filter(p => p.type === "react" && p.pla
 
 const allLinks = [...staticLinks, ...allNavLinks, ...allBlogs, ...allProjects]
 
-var siteMap = '<?xml-stylesheet type="text/xsl" href="https://www.sitemap.style/xslt/vanilla-water.xslt" ?>'
+var siteMap = '<?xml version="1.0" encoding="utf-8"?>'
+siteMap += '<?xml-stylesheet type="text/xsl" href="https://www.sitemap.style/xslt/vanilla-water.xslt" ?>'
 siteMap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
 allLinks.map(l => {
