8831249332c4e3b1caa545b89ef4f664dad2e8d3
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-20 17:02:52 +0530

    try set e

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
-if node generate-static-files.js; then
-  echo "Static file generated successfully"
-else
-  echo "Static file generation failed"
-  exit 1
-fi
-
-
-if node generate-sitemap.js; then
-  echo "Sitemap generated successfully"
-else
-  echo "Sitemap generation failed"
-  exit 1
-fi
-
-
-if node change-markdown.js; then
-  echo "Markdown variable extrapolated successfully"
-else
-  echo "Markdown variable extrapolation failed"
-  exit 1
-fi
+set -e
+node generate-static-files.js
+node generate-sitemap.js
+node change-markdown.js
 
 git add --all
