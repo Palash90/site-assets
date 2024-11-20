a4a3e369ec798c81cc740f55d862f27bb3404cd8
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-20 16:15:31 +0530

    Add type to draft blogs

--- site-contents/js/drafts.js
+++ site-contents/js/drafts.js
@@ -3,6 +3,7 @@ var drafts = [
         id: "setting-up-a-site",
         title: "Simple Static File Hosting",
         mdUrl: getCommon("cdn") + "/target/blogs/setting-up-a-site/README.md",
-        publishDate: "Nov 08, 2021"
+        publishDate: "Nov 08, 2021",
+        contentType:"tech"
     }
 ];
