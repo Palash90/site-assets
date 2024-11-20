4f914163e95a58af07421d888afcacd5992d4cb0
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-20 16:26:01 +0530

    Fix content type in draft

--- site-contents/js/drafts.js
+++ site-contents/js/drafts.js
@@ -4,6 +4,6 @@ var drafts = [
         title: "Simple Static File Hosting",
         mdUrl: getCommon("cdn") + "/target/blogs/setting-up-a-site/README.md",
         publishDate: "Nov 08, 2021",
-        contentType:"tech"
+        contentType:"swe"
     }
 ];
