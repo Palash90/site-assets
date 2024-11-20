07b80d8049813b18f91352953cdef41281294d00
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-11-20 16:47:58 +0530

    Try again

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 node generate-static-files.js
+if [ $? -eq 0 ]; then
+  echo "Node.js script executed successfully"
+else
+  echo "Static file generation failed"
+fi
+
 node generate-sitemap.js
+if [ $? -eq 0 ]; then
+  echo "Node.js script executed successfully"
+else
+  echo "Sitemap generation failed"
+fi
+
 node change-markdown.js
+if [ $? -eq 0 ]; then
+  echo "Node.js script executed successfully"
+else
+  echo "Markdown variable extrapolation failed"
+fi
+
 git add --all
