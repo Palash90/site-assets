61ce53e5847b3705cb115e7069bf02778a109c92
palash90 <palash.kanti.kundu.90@gmail.com>
2024-12-03 18:27:59 +0530

    draft the new blog

--- site-contents/js/drafts.js
+++ site-contents/js/drafts.js
@@ -5,6 +5,12 @@ var drafts = [
         mdUrl: getCommon("cdn") + "/target/blogs/setting-up-a-site/README.md",
         publishDate: "Nov 08, 2021",
         contentType:"swe"
+    },    {
+        id: "setting-up-a-site-4",
+        title: "Simple Static File Hosting",
+        mdUrl: getCommon("cdn") + "/target/blogs/setting-up-a-site/part-4.md",
+        publishDate: "Nov 08, 2021",
+        contentType:"swe"
     },
     {
         id: "distributed-systems",
