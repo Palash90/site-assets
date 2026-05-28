356b8897566ca97cff29f34a3527e73dc5e2d198
palash90 <palash.kanti.kundu.90@gmail.com>
2026-05-28 20:41:36 +0530

    Add guitalele tab and notes for sliding shape guitalele blog post

--- site-contents/js/musicBlogs.js
+++ site-contents/js/musicBlogs.js
@@ -3,7 +3,11 @@ var musicContents = [
         id: "sliding-shape-guitalele",
         publishDate: "Oct 16, 2024",
         title: "Sliding Shape Guitalele",
-        videoId: "hMBpAPGqX8k"
+        videoId: "hMBpAPGqX8k",
+        guitalele: {
+            tabMd: "E|--0--2--4--\nB|--1--3--5--\nG|--0--2--4--\nD|--2--4--6--\nA|--3--5--7--\nE|--x--x--x--\n",
+            notes: "Tuning: ADGCEA\nTempo: 90 BPM"
+        }
     },
     {
         id: "guitalele-tuning",
